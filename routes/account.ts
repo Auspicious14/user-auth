6b6c69cd2f0c680dd55f281828402325ba290754
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-03-16 17:46:17 +0100

    fixed and tested getting all users

--- routes/account.ts
+++ routes/account.ts
@@ -2,5 +2,5 @@ import express from "express";
 import { getUsers } from "../controllers/account";
 const router = express.Router();
 
-router.post("/account/", getUsers);
+router.get("/account", getUsers);
 export default router;
--- routes/userAuth.ts
+++ routes/userAuth.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { getUsers } from "../controllers/account";
 import { createUserAuth, loginUserAuth } from "../controllers/userAuth";
 const router = express.Router();
 
 router.post("/auth/signup", createUserAuth);
 router.post("auth/login", loginUserAuth);
+router.get("/account", getUsers);
 export default router;
